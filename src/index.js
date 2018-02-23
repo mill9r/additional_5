d143be4342c1473390653cf4f215885579064048
TATJAVA_Andrei_Tsylko <mi113r@mail.ru>
2018-02-23 22:55:08 +0300

    Update index.js
    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+        while (str.length > 0) {
+            prev_str = str;
+            str = remove_braice_pair(str, bracketsConfig);
+
+            if (str === prev_str) {
+                return false
+            }
+        }
+
+        return true
+
+    }
+
+function remove_braice_pair(input_string, conf) {
+    conf.forEach(function (i) {
+        repl = i.join('');
+        input_string = input_string.replace(repl, '');
+    });
+    return input_string
+
 }
